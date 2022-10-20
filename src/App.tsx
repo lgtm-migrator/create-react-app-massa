c834048b5cff9d41afc745f0683787390bb4fe4d
EvgeniPirianov <evgeni.pirianov@level01.io>
2022-10-20 19:28:00 +0300

    updated react version and removed massa wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import './App.css';
 import massa from './massa-logo.png'
-import MassaContext from './context';
-import MassaDappExample from './MassaDappExample';
-import { IContext } from '@massalabs/massa-react-wallet';
+import {MassaDappCore} from './MassaDappExample';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <MassaContext.Provider value={{} as IContext}>
         <header className="App-header">
           <img src={massa} className="App-logo" alt="logo"/>
-          <MassaDappExample />
+          <MassaDappCore />
         </header>
-      </MassaContext.Provider>
     </div>
   );
 }
--- src/context.ts
+++
@@ -1,6 +0,0 @@
-import { createMassaContext } from "@massalabs/massa-react-wallet";
-
-// create global massa context
-const MassaContext = createMassaContext(null);
-
-export default MassaContext;
